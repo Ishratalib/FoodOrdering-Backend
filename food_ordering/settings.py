67b12b43d2359686903dd79774b01f45cfff7b90
ishratalib <ishrat4561@gmail.com>
2026-07-22 11:50:23 +0500

    Add django-cors-headers configuration

--- food_ordering/settings.py
+++ food_ordering/settings.py
@@ -52,6 +52,8 @@ USE_X_FORWARDED_HOST = True
 # Application definition
 
 INSTALLED_APPS = [
+
+     "corsheaders",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
